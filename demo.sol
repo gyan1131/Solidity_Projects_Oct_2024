2d1e74e4b024ad846b047fdf87792d13151d6a1d
gyan1131 <gyanendra1131singh@gmail.com>
2024-10-18 21:09:01 +0530

    Solidity_demo_project

---
+++ demo.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract identity 
+
+{
+   string public name;
+   string public country;
+   uint256 public age;
+
+   constructor ()  
+   {
+    name="Sandy";
+    age=34; are
+    country="india";
+   }
+function getName() view public returns(string memory)
+{
+    return name;
+}
+function getAge() view public returns(uint)
+{
+    return age;
+}
+function getCountry() view public returns(string memory )
+{
+    return country;
+}
+
+
+
+   
+}
